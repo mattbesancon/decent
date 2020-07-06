6333893a79113aaa5fc0625b78b4198330e586cc
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-07-06 21:35:27 +0200

    post controller with associated CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,24 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+  
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def create
+    @post = Post.new(post_params)
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:tittle, :author, :comment, :rating)
   end
 end
