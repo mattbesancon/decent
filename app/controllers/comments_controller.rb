e5e8fe949907bd633daaac94d890fbe99925c409
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-07-06 21:38:04 +0200

    comment controller with associated CRUD

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,18 @@
 class CommentsController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.post = @post
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render 'posts/show'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :author)
   end
 end
