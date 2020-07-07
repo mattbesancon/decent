197161055d79be09b3aa683ca44c4d70e53cb375
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-07-07 21:08:44 +0200

    adding scss for card & seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+post_list = [
+    ["Post test 1", "Author test 1", "Content test 1", 0],
+    ["Post test 2", "Author test 2", "Content test 2", 0],
+    ["Post test 3", "Author test 3", "Content test 3", 0]
+];
+  
+post_list.each do |title, author, content, rating|
+    Post.create(title: title, author: author, content: content, rating: rating)
+end
