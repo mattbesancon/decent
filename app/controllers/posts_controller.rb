3132be92788dfeeb7389223710c6606abed28f27
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-07-08 16:02:49 +0200

    adding save for posts#create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,11 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
   end
 
   private
