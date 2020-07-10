76cc1c9973a432862a33314d8c96357529c9f5b7
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-07-10 21:57:24 +0200

    comments about the use of validates-with

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,20 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :comments, dependent: :destroy
     has_one_attached :photo
+    # include ActiveModel::Validations
+    # validates_with MyValidator
 end
+
+=begin
+class MyValidator < ActiveModel::Validator
+    def validate(record)
+        if some_complex_logic
+        record.errors.add :base, 'This record is invalid'
+        end
+    end
+
+private
+    def some_complex_logic
+    end
+end
+=end
