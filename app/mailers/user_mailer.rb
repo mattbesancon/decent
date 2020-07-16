31ae0160487de506e666e334ddcb70c68c56cc00
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-07-16 18:17:19 +0200

    user_id for mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
-    def verification_email
-        @user = params[:user]
+    def verification_email(user_id)
+        @user = User.find(user_id)
         mail(to: @user.email, subject: 'Your post now needs to be validated')
     end
 end
