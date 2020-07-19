4a10abee7b2c9ad003f2f34fbd86b30e595fd8c4
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-07-19 21:27:52 +0200

    send_verification_email for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   has_many :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  after_create :send_verification_email
+
+  private
+
+  def send_verification_email
+    # @user = params[:user] if current_user does not work
+    UserMailer.with(user: current_user).verification_email.deliver_now
+  end
 end
