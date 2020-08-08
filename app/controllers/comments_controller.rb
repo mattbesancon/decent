37ef8a7f46e129c78233a4b9cb3852d0cf79847e
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-08-08 14:53:21 +0200

    remove author in controller & view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :author)
+    params.require(:comment).permit(:content)
   end
 end
