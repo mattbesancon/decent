7135153a82352b39bc5690a3e949701ff200d951
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-08-11 21:22:01 +0200

    handle image when not attached

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,4 @@ class Post < ApplicationRecord
     validates :title, :author, :content, presence: true
     has_many :comments, dependent: :destroy
     has_one_attached :photo
-    delegate :key, to: :photo, allow_nil: true
 end
