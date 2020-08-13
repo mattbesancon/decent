a91fe40c57ea8258b9a1341c6c6222463ac83929
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-08-13 18:18:46 +0200

    validations models test for post & user

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = Post.new(title: 'test title', author: 'test author', content: 'test content')
+  end
+  
+  test 'valid post' do
+    assert @post.valid?
+  end
+  
+  test 'invalid without title' do
+    @post.title= nil
+    refute @post.valid?, 'saved user without a title'
+    assert_not_nil @post.errors[:title], 'no validation error for email present'
+  end
+  
+  test 'invalid without author' do
+    @post.author = nil
+    refute @post.valid?
+    assert_not_nil @post.errors[:author]
+  end
+
+  test 'invalid without content' do
+    @post.content = nil
+    refute @post.valid?
+    assert_not_nil @post.errors[:content]
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(email: 'john@example.com', password: 'password')
+  end
+  
+  test 'valid user' do
+    assert @user.valid?
+  end
+  
+  test 'invalid without email' do
+    @user.email = nil
+    refute @user.valid?, 'saved user without an email'
+    assert_not_nil @user.errors[:email], 'no validation error for email present'
+  end
+  
+  test 'invalid without password' do
+    @user.password = nil
+    refute @user.valid?
+    assert_not_nil @user.errors[:password]
+  end
 end
