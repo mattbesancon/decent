6e12fb9ba16c5c10d1cde71dac3bf81c456c7825
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-08-14 15:46:46 +0200

    comment test without valid comment

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @comment = Comment.new(content: 'test content', post_id: 2)
+  end
+  
+  #test 'valid comment' do
+  #  assert @comment.valid?
+  #end
+  
+  test 'invalid without content' do
+    @comment.content = nil
+    refute @comment.valid?, 'saved comment without a content'
+    assert_not_nil @comment.errors[:content], 'no validation error for content present'
+  end
+
+  test 'invalid without post_id' do
+    @comment.post_id = nil
+    refute @comment.valid?, 'saved comment without post_id'
+    assert_not_nil @comment.errors[:post_id], 'no validation error for post_id present'
+  end
 end
