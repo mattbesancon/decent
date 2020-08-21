1a48af3ffa39ad91e22ee2be040218e088d1bf2b
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-08-21 16:12:02 +0200

    select verified from index & remove migration add_admin_to_users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 seed_list = [
-    ["Post test 1", "Author test 1", "Content test 1"],
+    ["Post test 1", "Author test 1", "Content test 1",true],
     ["Post test 2", "Author test 2", "Content test 2"],
     ["Post test 3", "Author test 3", "Content test 3"],
     ["Post test 4", "Author test 4", "Content test 4"],
@@ -18,7 +18,7 @@ seed_list = [
     ["Post test 9", "Author test 9", "Content test 9"],
     ["Post test 10", "Author test 10", "Content test 10"],
     ["Post test 11", "Author test 11", "Content test 11"],
-    ["Post test 12", "Author test 12", "Content test 12"],
+    ["Post test 12", "Author test 12", "Content test 12", true],
     ["Post test 13", "Author test 13", "Content test 13"],
     ["Post test 14", "Author test 14", "Content test 14"],
     ["Post test 15", "Author test 15", "Content test 15"],
@@ -28,7 +28,7 @@ seed_list = [
     ["Post test 19", "Author test 19", "Content test 19"],
     ["Post test 20", "Author test 20", "Content test 20"],
     ["Post test 21", "Author test 21", "Content test 21"],
-    ["Post test 22", "Author test 22", "Content test 22"],
+    ["Post test 22", "Author test 22", "Content test 22", true],
     ["Post test 23", "Author test 23", "Content test 23"],
     ["Post test 24", "Author test 24", "Content test 24"],
     ["Post test 25", "Author test 25", "Content test 25"],
@@ -41,6 +41,6 @@ seed_list = [
     ["Post test 32", "Author test 32", "Content test 32"]
 ];
 
-seed_list.each do |title, author, content|
-    Post.create(title: title, author: author, content: content)
+seed_list.each do |title, author, content, verified|
+    Post.create(title: title, author: author, content: content, verified: verified)
 end
