ef39cecdf9c2b46d28d5e5e45613a2c2719d7687
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-08-25 16:24:43 +0200

    work on destroy and update for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :posts, only: [:index, :show, :create, :new] do
+  resources :posts, only: [:index, :show, :create, :new, :edit, :update, :destroy] do
     resources :comments, only: [:create]
   end
 end
